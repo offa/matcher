28146127e4e85a0d6ac28cc40a70f501e7bf82b9
offa <bm-dev@yandex.com>
2016-03-30 18:14:51 +0200

    Header added, which includes all necessary headers.

---
+++ include/matcher/Matcher.h
@@ -0,0 +1,13 @@
+
+#ifndef MATCHER_H
+#define MATCHER_H
+
+#include "Actual.h"
+#include "CombinationMatcher.h"
+#include "CompareMatcher.h"
+#include "ContainerMatcher.h"
+#include "FloatMatcher.h"
+#include "StringMatcher.h"
+
+#endif /* MATCHER_H */
+
